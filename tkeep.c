66e8b083ae922403e30037ef1687f9834c2e04d9
Nic <nicq20@gmail.com>
2018-03-05 12:55:41 -0500

    Get the basics worked out.

--- tkeep.c
+++ tkeep.c
@@ -10,11 +10,22 @@ Time keeper. Get the current time and write it to a file.
 #include <string.h>
 
 
+/* Define marcos */
+#define opt(b) (strcmp(argv[i], b) == 0)
+#define debug(msg) (printf("%s\n", msg))
+
+
+/* Prototypes */
+void help();
+char * isotime();
+int write(char *, int);
+
+
 /* Main */
 int main(int argc, char const *argv[]) {
 	/* Makes some vars */
 	char * loc = getenv("TSLOC");
-	time_t t = time(0);
+	int s = 0;
 
 	/* Give a default to `loc` if the 'TSLOC' variable is not set. */
 	if (loc == NULL) {
@@ -22,11 +33,40 @@ int main(int argc, char const *argv[]) {
 		strcat(loc, "/Documents/ts");
 	}
 
-	for (int i = 0; i < argc; ++i) {
-		/**/
+	for (int i = 1; i < argc; ++i) {
+		if (opt("-h") || opt("--help")) { help(); }
+		else
+		if (opt("-i") || opt("--in")  ) { write(loc, 0); }
+		else
+		if (opt("-o") || opt("--out") ) { write(loc, 1); }
+		else
+			{ fprintf(stderr, "Unknown argument: %s\n", argv[i]); exit(1); }
 	}
 
 	printf("%s", loc);
 
+	return 0;
+}
+
+
+/* Help message */
+void help() {
+	printf("USAGE: tkeep [-i|--in] [-o|--out]\n");
+}
+
+
+/* Get a string of the current time in iso format. */
+char * isotime() {
+	time_t t = time(0);
+	t = (t - ( (t - 7) % (15 * 60) ) );
+	return asctime( localtime( &t ) );
+}
+
+
+/* Write time to file. */
+int write(char * f, int s) {
+	FILE *fh = fopen(f, "w");
+	fprintf(fh, "%d,%s", s, isotime());
+	fclose(fh);
 	return 0;
 }
