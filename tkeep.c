7a8316145e31085289022f73931f5edf38ff56a1
Nic <nicq20@gmail.com>
2018-03-06 10:50:34 -0500

    Fix the time string, and append to the file.

--- tkeep.c
+++ tkeep.c
@@ -7,7 +7,6 @@ Time keeper. Get the current time and write it to a file.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define __USE_XOPEN
 #include <time.h>
 
 
@@ -34,17 +33,21 @@ int main(int argc, char const *argv[]) {
 		strcat(loc, "/Documents/ts");
 	}
 
+	/* Display help if no arguments were given. */
+	if (argc == 1) { help(); }
+
+	/* Go through the arguments given. */
 	for (int i = 1; i < argc; ++i) {
 		if (opt("-h") || opt("--help")) { help(); }
 		else
 		if (opt("-i") || opt("--in")  ) { write(loc, 0); }
 		else
 		if (opt("-o") || opt("--out") ) { write(loc, 1); }
-		else
-			{ fprintf(stderr, "Unknown argument: %s\n", argv[i]); exit(1); }
+		else {
+			fprintf(stderr, "Unknown argument: %s\n", argv[i]);
+			exit(1);
+		}
 	}
-
-	printf("%s", loc);
 
 	return 0;
 }
@@ -52,7 +55,7 @@ int main(int argc, char const *argv[]) {
 
 /* Help message */
 void help() {
-	printf("USAGE: tkeep [-i|--in] [-o|--out]\n");
+	printf("USAGE: tkeep [-h|--help] [-i|--in] [-o|--out]\n");
 }
 
 
@@ -60,7 +63,7 @@ void help() {
 char * isotime(char * str) {
 	time_t t = time(0);
 	t = (t - ( (t - 7) % (15 * 60) ) );
-	strptime(str, "%FT%T%z", localtime(&t));
+	strftime(str, 64, "%FT%T%z", localtime(&t));
 	return str;
 }
 
@@ -68,7 +71,7 @@ char * isotime(char * str) {
 /* Write time to file. */
 int write(char * f, int s) {
 	char iso[64];
-	FILE *fh = fopen(f, "w");
+	FILE *fh = fopen(f, "a");
 
 	if (fh == NULL) {
 		fprintf(stderr, "Can not write to '%s'\n", f);
@@ -76,9 +79,8 @@ int write(char * f, int s) {
 	}
 
 	isotime(iso);
-	fprintf(fh, "%d,%s", s, iso);
+	fprintf(fh, "%d,%s\n", s, iso);
 
 	fclose(fh);
 	return 1;
 }
-
