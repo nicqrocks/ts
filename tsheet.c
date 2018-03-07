34d98a653e2fd09b5d7b86a2262dab4613abde6e
Nic Q <nicq20@gmail.com>
2018-03-07 14:33:19 -0500

    Add macros to tsheet

--- tsheet.c
+++ tsheet.c
@@ -1,5 +1,6 @@
 /*
 Nic Q
+Time sheet. Read the time log and output a time sheet in CSV format.
 2018/03/07
 */
 
@@ -9,6 +10,16 @@ Nic Q
 #include <time.h>
 
 
-int main() {
+/* Define marcos */
+#define chkopt(a)	(strcmp(argv[i], a) == 0)
+#define dscopt(a,b)	(printf("\t%s\n\t\t%s\n", a, b))
+#define debug(msg)	(printf("%s\n", msg))
+
+
+/* Prototypes */
+void help();
+
+
+int main(int argc, char const *argv[]) {
 	return 0;
 }
